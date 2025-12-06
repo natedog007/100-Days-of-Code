cf2b4f49f3db55793bc589f1ea80fccb38410ae0
Nathan Boyden <nrb121903@gmail.com>
2025-12-06 06:21:56 -0500

    e

---
+++ Day 5 - Password Generator/main.py
@@ -0,0 +1,13 @@
+
+# Raw chrarcters for password generation
+symbols = []
+letters = []
+nums = []
+
+# Data collection
+print("Welcome to the PyPassword Generator!") 
+num_letters = input("How many letters would you like in your password?\n")
+num_symbols = input()
+num_nums = input()
+
+# Password Creatione
